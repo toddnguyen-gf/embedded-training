a35dc180aff9bb3a2e137ca5309b4e02233f3298
Todd Nguyen <todd.nguyen@georgfischer.com>
2021-03-10 17:15:59 -0800

    FInish 2.2 Hardware section

--- rust-embedded-book/02-getting-started/app/examples/hello.rs
+++ rust-embedded-book/02-getting-started/app/examples/hello.rs
@@ -3,18 +3,22 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 use panic_halt as _;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::{debug, hio, hprintln};
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
+    // hprintln!("Hello, world!").unwrap();
+    let mut stdout = hio::hstdout().unwrap();
+    writeln!(stdout, "Hello, world!").unwrap();
 
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
+    // debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
 }
--- rust-embedded-book/02-getting-started/app/scripts/run-debugger.bat
+++ rust-embedded-book/02-getting-started/app/scripts/run-debugger.bat
@@ -1,1 +1,4 @@
-arm-none-eabi-gdb -q target/thumbv7m-none-eabi/debug/examples/hello
+@echo off
+set "file_name=target/thumbv7em-none-eabihf/debug/examples/hello"
+echo Debugging: '%file_name%'
+arm-none-eabi-gdb -q %file_name%
