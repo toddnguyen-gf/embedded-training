43fad00f844ed09161c44ef2f4ed4729e7dfdb46
Todd Nguyen <todd.nguyen@georgfischer.com>
2021-03-10 18:45:30 -0800

    Exceptions complete example

--- rust-embedded-book/02-getting-started/app/src/main.rs
+++ rust-embedded-book/02-getting-started/app/src/main.rs
@@ -1,21 +1,54 @@
-#![no_std]
+#![deny(unsafe_code)]
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-                     // use panic_abort as _; // requires nightly
-                     // use panic_itm as _; // logs messages over ITM; requires ITM support
-                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+use core::fmt::Write;
 
-// Use a divergent function `-> !`
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m_rt::{entry, exception};
+use cortex_m_semihosting::{
+    debug,
+    hio::{self, HStdout},
+};
+
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let p = cortex_m::Peripherals::take().unwrap();
+    let mut syst = p.SYST;
 
-    loop {
-        // your code goes here
+    // configures the system timer to trigger a SysTick exception every second
+    syst.set_clock_source(SystClkSource::Core);
+    // this is configured for the LM3S6965 which has a default CPU clock of 12 MHz
+    syst.set_reload(12_000_000);
+    syst.clear_current();
+    syst.enable_counter();
+    syst.enable_interrupt();
+
+    loop {}
+}
+
+#[exception]
+fn SysTick() {
+    static mut COUNT: u32 = 0;
+    static mut STDOUT: Option<HStdout> = None;
+
+    *COUNT += 1;
+
+    // Lazy initialization
+    if STDOUT.is_none() {
+        *STDOUT = hio::hstdout().ok();
+    }
+
+    if let Some(hstdout) = STDOUT.as_mut() {
+        write!(hstdout, "{}", *COUNT).ok();
+    }
+
+    // IMPORTANT omit this `if` block if running on real hardware or your
+    // debugger will end in an inconsistent state
+    if *COUNT == 9 {
+        // This will terminate the QEMU process
+        debug::exit(debug::EXIT_SUCCESS);
     }
 }
