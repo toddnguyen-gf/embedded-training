e82311c936714b0043c7aae65eb48006fd5e5e05
Todd Nguyen <todd.nguyen@georgfischer.com>
2021-03-12 18:06:56 -0800

    Finish with Proj1!

--- ece-18-642/lec-1-to-4/project1/turtle_publisher_18642.cpp
+++ ece-18-642/lec-1-to-4/project1/turtle_publisher_18642.cpp
@@ -18,16 +18,22 @@
 #include "geometry_msgs/Twist.h" // access using geometry_msgs::Twist
 #include "turtlesim/Pose.h"      // access using turtlesim::Pose
 
+#include <string>
+#include <sstream>
+
 // any state variables or callback/computation functions go here
 void draw_straight_line(ros::Publisher vel_pub);
 void draw_eight_figure(ros::Publisher vel_pub);
+void callback(const turtlesim::Pose &msg);
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "turtle_publisher_18642");
   ros::NodeHandle nh;
 
-  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+  int queue_size = 1000;
+  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", queue_size);
+  ros::Subscriber sub = nh.subscribe("/turtle1/pose", queue_size, callback);
 
   ROS_INFO("Starting up turtle_publisher_18642.");
 
@@ -38,6 +44,7 @@ int main(int argc, char **argv)
   // draw_straight_line(vel_pub);
   draw_eight_figure(vel_pub);
 
+  ros::spin();
   return 0;
 }
 
@@ -59,8 +66,9 @@ void draw_straight_line(ros::Publisher vel_pub)
 
 void draw_eight_figure(ros::Publisher vel_pub)
 {
-  ros::Rate r(.5); // .5Hz (run every 2 seconds)
-  int num_step = 8;
+  // ros::Rate r(.5); // .5Hz (run every 2 seconds)
+  ros::Rate r(1); // 1Hz (run every second)
+  int num_step = 7;
 
   // Figure 8 top
   for (int i = 0; i < num_step; i++)
@@ -83,4 +91,16 @@ void draw_eight_figure(ros::Publisher vel_pub)
     ros::spinOnce();
     r.sleep();
   }
+}
+
+/**
+ *  turtlesim::Pose found in `/opt/ros/kinetic/include/turtlesim`
+ */
+void callback(const turtlesim::Pose &msg)
+{
+  // x position, y position, linear velocity, angular velocity
+  std::ostringstream o_str_stream;
+  o_str_stream << "(x: " << msg.x << ", y: " << msg.y << ", linear velocity: " << msg.linear_velocity
+               << ", angular velocity: " << msg.angular_velocity << ")";
+  ROS_INFO("%s", o_str_stream.str().c_str());
 }
