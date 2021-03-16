400834de1056d7a705e1b2504d4a0277185f78b2
Todd Nguyen <todd.nguyen@georgfischer.com>
2021-03-15 17:09:02 -0700

    led roulette: Use volatile for GDB setting values

--- rust-embedded-discovery/discovery/scripts/openocd-in-temp.bat
+++ rust-embedded-discovery/discovery/scripts/openocd-in-temp.bat
@@ -1,3 +1,3 @@
 @echo off
 cd %TEMP%
-"C:\Users\i1A771792\AppData\Roaming\xPacks\@xpack-dev-tools\openocd\0.10.0-15.1\.content\bin\openocd.exe" -s C:\share\scripts -f interface/stlink-v2-1.cfg -f target/stm32f3x.cfg
+"C:\Users\i1A771792\AppData\Roaming\xPacks\@xpack-dev-tools\openocd\0.10.0-15.1\.content\bin\openocd.exe" -s C:\share\scripts -f interface/stlink.cfg -f target/stm32f3x.cfg
