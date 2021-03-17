b6c57801f8fddc5268b662264789f1bca5438baa
Todd Nguyen <todd.nguyen@georgfischer.com>
2021-03-16 19:03:55 -0700

    Use C's static instead of C++
    - The course calls for C, so...

--- ece-18-642/lec-05-to-09/proj03/p03_noid_student_turtle.cpp
+++ ece-18-642/lec-05-to-09/proj03/p03_noid_student_turtle.cpp
@@ -29,165 +29,111 @@ turtleMove studentTurtleStep(bool bumped) { return MOVE; }
  */
 bool studentMoveTurtle(QPointF &pos_, int &nw_or);
 
-#ifndef NOID_STUDENT_TURTLE_CLASS_H_
-#define NOID_STUDENT_TURTLE_CLASS_H_
-class StudentTurtle {
- public:
-  StudentTurtle();
-  ~StudentTurtle();
+typedef uint16_t Timeout;
 
-  typedef uint16_t Timeout;
+typedef struct Coordinates {
+  uint8_t x;
+  uint8_t y;
 
-  /**
-   * Move the turtle using the left hand rule.
-   */
-  bool moveLeftHandRule(QPointF &pos_, int &nw_or);
+  // Initialization list!
+  Coordinates() : x(0), y(0) {}
+} Coordinates;
 
-  /**
-   * Move the turtle using the right hand rule.
-   */
-  bool moveRightHandRule(QPointF &pos_, int &nw_or);
+typedef enum Orientation {
+  kWest = 0,
+  kNorth,
+  kEast,
+  kSouth,
+} Orientation;
 
-  const static Timeout TIMEOUT_MAX = 40;
+typedef enum State {
+  kNoWall = 0,
+  kHasWall,
+  kGoThisOrientation,
+} State;
 
- private:
-  typedef struct Coordinates {
-    uint8_t x;
-    uint8_t y;
+/**
+ * Move the turtle using the left hand rule.
+ */
+bool moveLeftHandRule(QPointF &pos_, int &nw_or);
 
-    // Initialization list!
-    Coordinates() : x(0), y(0) {}
-  } Coordinates;
-  typedef enum Orientation {
-    kWest = 0,
-    kNorth,
-    kEast,
-    kSouth,
-  } Orientation;
+/**
+ * Move the turtle using the right hand rule.
+ */
+bool moveRightHandRule(QPointF &pos_, int &nw_or);
 
-  typedef enum State {
-    kNoWall = 0,
-    kHasWall,
-    kGoThisOrientation,
-  } State;
+/** bigger number slows down simulation so you can see what's happening */
+static const Timeout TIMEOUT_MAX = 40;
+static Timeout _current_timeout;
 
-  const char *STATE_STR[3] = {"NO WALL", "HAS A WALL", "GO THIS ORIENTATION"};
-  const char *ORIENTATION_STR[4] = {"WEST", "NORTH", "EAST", "SOUTH"};
-  Timeout _current_timeout;
-  Coordinates _upper_left_coords, _lower_right_coords;
-  State _current_state;
-  bool _has_this_orientation_been_traveled, _is_end_of_maze,
-      _maze_contains_wall;
+static Coordinates _upper_left_coords, _lower_right_coords;
+static bool _has_this_orientation_been_traveled, _is_end_of_maze,
+    _maze_contains_wall;
 
-  /**
-   * Set the orientation and the current state base on the turtle's CURRENT
-   * orientation.
-   */
-  Orientation _setOrientationAndCurrentState(
-      Orientation orientation, Orientation orientationGoThisOrientation,
-      Orientation orientationMazeContainsWall);
+static const char *ORIENTATION_STR[] = {"WEST", "NORTH", "EAST", "SOUTH"};
 
-  /**
-   * Calculate and return if timeout is 0 for the next movement.
-   */
-  bool _returnTimeout();
+static const char *STATE_STR[] = {"NO WALL", "HAS A WALL",
+                                  "GO THIS ORIENTATION"};
+static State _current_state;
 
-  /**
-   * Move the turtle base on its orientation.
-   */
-  void _moveTurtleHelper(QPointF &pos_, Orientation orientation);
+/**
+ * Set the orientation and the current state base on the turtle's CURRENT
+ * orientation.
+ */
+static Orientation _setOrientationAndCurrentState(
+    Orientation orientation, Orientation orientationGoThisOrientation,
+    Orientation orientationMazeContainsWall);
 
-  /**
-   * Actually set the turtle's position e.g. have its y incremented if we are
-   * moving SOUTH.
-   */
-  void _setTurtlePosition(QPointF &pos_, Orientation orientation);
-};
-#endif  // NOID_STUDENT_TURTLE_CLASS_H_
+/**
+ * Calculate and return if timeout is 0 for the next movement.
+ */
+static bool _returnTimeout();
 
-static StudentTurtle _student_turtle;
+/**
+ * Move the turtle base on its orientation.
+ */
+static void _moveTurtleHelper(QPointF &pos_, Orientation orientation);
+
+/**
+ * Actually set the turtle's position e.g. have its y incremented if we are
+ * moving SOUTH.
+ */
+static void _setTurtlePosition(QPointF &pos_, Orientation orientation);
+
+// --------------------------------------------------------
+// | FUNCTION IMPLEMENTATION
+// --------------------------------------------------------
 
 bool studentMoveTurtle(QPointF &pos_, int &nw_or) {
-  // return _student_turtle.moveLeftHandRule(pos_, nw_or);
-  return _student_turtle.moveRightHandRule(pos_, nw_or);
+  // return moveLeftHandRule(pos_, nw_or);
+  return moveRightHandRule(pos_, nw_or);
 }
 
-// --------------------------------------------------------
-// |  IMPLEMENTATION
-// --------------------------------------------------------
-
-StudentTurtle::StudentTurtle() : _current_timeout(0) {}
-StudentTurtle::~StudentTurtle() {}
-
-bool StudentTurtle::moveLeftHandRule(QPointF &pos_, int &nw_or) {
+bool moveRightHandRule(QPointF &pos_, int &nw_or) {
   // ROS_INFO("Turtle update Called  w=%f", _current_timeout);
   Orientation orientation = static_cast<Orientation>(nw_or);
 
   if (0 == _current_timeout) {
-    this->_setTurtlePosition(pos_, orientation);
-    this->_maze_contains_wall =
-        bumped(_upper_left_coords.x, _upper_left_coords.y,
-               _lower_right_coords.x, _lower_right_coords.y);
-    this->_is_end_of_maze = atend(pos_.x(), pos_.y());
+    _setTurtlePosition(pos_, orientation);
+    _maze_contains_wall = bumped(_upper_left_coords.x, _upper_left_coords.y,
+                                 _lower_right_coords.x, _lower_right_coords.y);
+    _is_end_of_maze = atend(pos_.x(), pos_.y());
 
     switch (orientation) {
       case Orientation::kWest:
-        orientation = this->_setOrientationAndCurrentState(
-            orientation, Orientation::kSouth, Orientation::kNorth);
-        break;
-      case Orientation::kNorth:
-        orientation = this->_setOrientationAndCurrentState(
-            orientation, Orientation::kWest, Orientation::kEast);
-        break;
-      case Orientation::kEast:
-        orientation = this->_setOrientationAndCurrentState(
-            orientation, Orientation::kNorth, Orientation::kSouth);
-        break;
-      case Orientation::kSouth:
-        orientation = this->_setOrientationAndCurrentState(
-            orientation, Orientation::kEast, Orientation::kWest);
-        break;
-      default:
-        ROS_ERROR("%s", "Should never reach this default case");
-        break;
-    }
-
-    ROS_INFO("Orientation='%s' State='%s'",
-             ORIENTATION_STR[static_cast<int>(orientation)],
-             STATE_STR[static_cast<int>(_current_state)]);
-    this->_moveTurtleHelper(pos_, orientation);
-  }
-
-  nw_or = static_cast<int>(orientation);
-  return this->_returnTimeout();
-}
-
-bool StudentTurtle::moveRightHandRule(QPointF &pos_, int &nw_or) {
-  // ROS_INFO("Turtle update Called  w=%f", _current_timeout);
-  Orientation orientation = static_cast<Orientation>(nw_or);
-
-  if (0 == _current_timeout) {
-    this->_setTurtlePosition(pos_, orientation);
-    this->_maze_contains_wall =
-        bumped(_upper_left_coords.x, _upper_left_coords.y,
-               _lower_right_coords.x, _lower_right_coords.y);
-    this->_is_end_of_maze = atend(pos_.x(), pos_.y());
-
-    switch (orientation) {
-      case Orientation::kWest:
-        orientation = this->_setOrientationAndCurrentState(
+        orientation = _setOrientationAndCurrentState(
             orientation, Orientation::kNorth, Orientation::kSouth);
         break;
       case Orientation::kNorth:
-        orientation = this->_setOrientationAndCurrentState(
+        orientation = _setOrientationAndCurrentState(
             orientation, Orientation::kEast, Orientation::kWest);
         break;
       case Orientation::kEast:
-        orientation = this->_setOrientationAndCurrentState(
+        orientation = _setOrientationAndCurrentState(
             orientation, Orientation::kSouth, Orientation::kNorth);
         break;
       case Orientation::kSouth:
-        orientation = this->_setOrientationAndCurrentState(
+        orientation = _setOrientationAndCurrentState(
             orientation, Orientation::kWest, Orientation::kEast);
         break;
       default:
@@ -199,47 +145,88 @@ bool StudentTurtle::moveRightHandRule(QPointF &pos_, int &nw_or) {
              ORIENTATION_STR[static_cast<int>(orientation)],
              STATE_STR[static_cast<int>(_current_state)]);
 
-    this->_moveTurtleHelper(pos_, orientation);
+    _moveTurtleHelper(pos_, orientation);
   }
 
   nw_or = static_cast<int>(orientation);
-  return this->_returnTimeout();
+  return _returnTimeout();
 }
 
-StudentTurtle::Orientation StudentTurtle::_setOrientationAndCurrentState(
+bool moveLeftHandRule(QPointF &pos_, int &nw_or) {
+  // ROS_INFO("Turtle update Called  w=%f", _current_timeout);
+  Orientation orientation = static_cast<Orientation>(nw_or);
+
+  if (0 == _current_timeout) {
+    _setTurtlePosition(pos_, orientation);
+    _maze_contains_wall = bumped(_upper_left_coords.x, _upper_left_coords.y,
+                                 _lower_right_coords.x, _lower_right_coords.y);
+    _is_end_of_maze = atend(pos_.x(), pos_.y());
+
+    switch (orientation) {
+      case Orientation::kWest:
+        orientation = _setOrientationAndCurrentState(
+            orientation, Orientation::kSouth, Orientation::kNorth);
+        break;
+      case Orientation::kNorth:
+        orientation = _setOrientationAndCurrentState(
+            orientation, Orientation::kWest, Orientation::kEast);
+        break;
+      case Orientation::kEast:
+        orientation = _setOrientationAndCurrentState(
+            orientation, Orientation::kNorth, Orientation::kSouth);
+        break;
+      case Orientation::kSouth:
+        orientation = _setOrientationAndCurrentState(
+            orientation, Orientation::kEast, Orientation::kWest);
+        break;
+      default:
+        ROS_ERROR("%s", "Should never reach this default case");
+        break;
+    }
+
+    ROS_INFO("Orientation='%s' State='%s'",
+             ORIENTATION_STR[static_cast<int>(orientation)],
+             STATE_STR[static_cast<int>(_current_state)]);
+    _moveTurtleHelper(pos_, orientation);
+  }
+
+  nw_or = static_cast<int>(orientation);
+  return _returnTimeout();
+}
+
+static Orientation _setOrientationAndCurrentState(
     Orientation orientation, Orientation orientationGoThisOrientation,
     Orientation orientationMazeContainsWall) {
-  if (this->_current_state == State::kGoThisOrientation) {
+  if (_current_state == State::kGoThisOrientation) {
     orientation = orientationGoThisOrientation;
-    this->_current_state = State::kHasWall;
-  } else if (this->_maze_contains_wall) {
+    _current_state = State::kHasWall;
+  } else if (_maze_contains_wall) {
     orientation = orientationMazeContainsWall;
-    this->_current_state = State::kNoWall;
+    _current_state = State::kNoWall;
   } else {
-    this->_current_state = State::kGoThisOrientation;
+    _current_state = State::kGoThisOrientation;
   }
   return orientation;
 }
 
-bool StudentTurtle::_returnTimeout() {
-  if (this->_is_end_of_maze) {
+static bool _returnTimeout() {
+  if (_is_end_of_maze) {
     return false;
   }
 
-  if (this->_current_timeout == 0) {
-    this->_current_timeout = StudentTurtle::TIMEOUT_MAX;
+  if (_current_timeout == 0) {
+    _current_timeout = TIMEOUT_MAX;
   } else {
-    this->_current_timeout -= 1;
+    _current_timeout -= 1;
   }
 
-  return StudentTurtle::TIMEOUT_MAX == this->_current_timeout;
+  return TIMEOUT_MAX == _current_timeout;
 }
 
-void StudentTurtle::_moveTurtleHelper(QPointF &pos_, Orientation orientation) {
-  this->_has_this_orientation_been_traveled =
-      this->_current_state == State::kGoThisOrientation;
-  if (this->_has_this_orientation_been_traveled == true &&
-      this->_is_end_of_maze == false) {
+static void _moveTurtleHelper(QPointF &pos_, Orientation orientation) {
+  _has_this_orientation_been_traveled =
+      _current_state == State::kGoThisOrientation;
+  if (_has_this_orientation_been_traveled == true && _is_end_of_maze == false) {
     switch (orientation) {
       case Orientation::kNorth:
         pos_.setY(pos_.y() - 1);
@@ -258,32 +245,32 @@ void StudentTurtle::_moveTurtleHelper(QPointF &pos_, Orientation orientation) {
         break;
     }
 
-    this->_has_this_orientation_been_traveled = false;
+    _has_this_orientation_been_traveled = false;
   }
 }
 
-void StudentTurtle::_setTurtlePosition(QPointF &pos_, Orientation orientation) {
-  this->_upper_left_coords.x = pos_.x();
-  this->_upper_left_coords.y = pos_.y();
-  this->_lower_right_coords.x = pos_.x();
-  this->_lower_right_coords.y = pos_.y();
+static void _setTurtlePosition(QPointF &pos_, Orientation orientation) {
+  _upper_left_coords.x = pos_.x();
+  _upper_left_coords.y = pos_.y();
+  _lower_right_coords.x = pos_.x();
+  _lower_right_coords.y = pos_.y();
 
   switch (orientation) {
     case Orientation::kWest:
-      this->_lower_right_coords.y += 1;
+      _lower_right_coords.y += 1;
       break;
     case Orientation::kNorth:
-      this->_lower_right_coords.x += 1;
+      _lower_right_coords.x += 1;
       break;
     case Orientation::kEast:
-      this->_lower_right_coords.x += 1;
-      this->_lower_right_coords.y += 1;
-      this->_upper_left_coords.x += 1;
+      _lower_right_coords.x += 1;
+      _lower_right_coords.y += 1;
+      _upper_left_coords.x += 1;
       break;
     case Orientation::kSouth:
-      this->_lower_right_coords.x += 1;
-      this->_lower_right_coords.y += 1;
-      this->_upper_left_coords.y += 1;
+      _lower_right_coords.x += 1;
+      _lower_right_coords.y += 1;
+      _upper_left_coords.y += 1;
       break;
     default:
       ROS_ERROR("%s", "Should never reach this default case");
